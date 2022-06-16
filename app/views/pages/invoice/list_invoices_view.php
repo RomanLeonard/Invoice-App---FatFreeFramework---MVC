da4edcdeeee70d7a30177caa7ef7e26f3f843184
Leonard Roman <romanleonard@outlook.com>
2022-06-16 17:57:48 +0300

    adding table

--- app/views/pages/invoice/list_invoices_view.php
+++ app/views/pages/invoice/list_invoices_view.php
@@ -1,14 +1,38 @@
 
-<div class="content">
-    <span>invoice_add_view</span>
+<div class="page-content">
+    <div class="card">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Number</th>
+                    <th>Date</th>
+                    <th>Client</th>
+                    <th>Items</th>
+                    <th style="width: 210px;">Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <repeat group="{{ @invoices['subset'] }}" value="{{ @invoice }}">
+                    <tr>
+                        <td>{{ @invoice->number }}</td>
+                        <td>{{ @invoice->date }}</td>
+                        <td>{{ @invoice->client_id }}</td>
+                        <td>{{ @invoice->items }}</td>
+                        <td>
+                            <div>
+                                <a class="btn btn-primary" href="#">edit</a>
+                                <a class="btn btn-dark" href="#">storno</a>
+                                <a class="btn btn-danger" href="#">cancel</a>
+                            </div>
+                        </td>
+                    </tr>
+                </repeat>
+
+                
+            </tbody>
+        </table>
+    </div>
 
 
 
-    <br><br>
-
-    <repeat group="{{ @invoices['subset'] }}" value="{{ @invoice }}">
-
-        <span> {{ @invoice->number }} </span>
-
-    </repeat>
 </div>
